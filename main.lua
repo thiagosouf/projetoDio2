d9001e0853deadf278cb7933542d7a8b38c6e285
Thiago Ferreira <thiagosouf@gmail.com>
2024-10-08 10:01:24 -0300

    teste

---
+++ main.lua
@@ -0,0 +1,7 @@
+--Habilitar UTF-8 no terminal
+os.execute("chcp 65001")
+
+-- Header
+print("==============================================")
+print("            SIMULADOR DE BATALHA              ")
+print("==============================================")
